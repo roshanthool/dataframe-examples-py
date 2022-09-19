4bf53de24b0238c2e9a9d8bcc058b82668c4065b
roshan <thoolroshan@gmail.com>
2022-09-19 08:48:27 +0530

    rdd examples

--- rdd/scholaship_recipient_join_filter.py
+++ rdd/scholaship_recipient_join_filter.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     join_pair_rdd.foreach(print)
 
     print('result')
-    join_pair_rdd.filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1)) \
+    join_pair_rdd = join_pair_rdd.filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1)) \
 
 
     join_pair_rdd.foreach(print)
